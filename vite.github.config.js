88bc1d7821cb285b1baaf2f7a635de26b88c3f5d
Litovic3253 <mpavlukevic@gmail.com>
2025-11-15 13:50:16 +0300

    Возвращен деплой через ветку gh-pages с поддиректорией /MP_Foto/

--- vite.github.config.js
+++ vite.github.config.js
@@ -1,28 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import { copyFileSync } from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "copy-cname",
-      closeBundle() {
-        try {
-          copyFileSync(
-            resolve(__dirname, "public", "cname"),
-            resolve(__dirname, "dist-github", "CNAME")
-          );
-          console.log("CNAME file copied successfully");
-        } catch (error) {
-          console.log("No cname file to copy");
-        }
-      },
-    },
-  ],
-  base: "/",
+  plugins: [react()],
+  base: "/MP_Foto/",
   build: {
     outDir: "dist-github",
   },
